ab52bac15816a143719475aa2908c17ad8353199
deng gong <rzpfox@gmail.com>
2018-11-19 14:20:27 +0800

    rails-stupid-coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @response = params[:question]
+    if @response == "I am going to work right now!"
+      @coach_answer = "Great!"
+    elsif @response.include? "?"
+      @coach_answer = "Silly question, get dressed and go to work!"
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
